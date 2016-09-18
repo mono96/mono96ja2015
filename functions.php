69df65dedeb902c31dde92abdef5ba5327d3ff17
NOBUHITO <nobuhito@NOBUHITO-no-MacBook-Pro.local>
2016-09-18 13:07:24 +0900

    first commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+//子テーマ 読み込み
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
+
+//管理画面  CSS 読み込み
+add_editor_style("editor-style.css");
---
+++ readme.txt
@@ -0,0 +1,10 @@
+mono96ja2015
+==============================
+mono96ja2015 is a WordPress theme that is the parent theme of Twenty Fifteen for Japanese.
+
+
+License
+--------------------
+
+mono96ja2015 WordPress Theme, Copyright 2016 Nobuhito Ohigashi
+mono96ja2015 is distributed under the terms of the GNU GPL
